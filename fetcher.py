bc6f27610faccc1249b7dab1fdaed88dd61e6572
sugardrunk <sugardrunk@hear.fi>
2011-04-06 11:24:20 +0300

    version 0.0.9

--- fetcher.py
+++ fetcher.py
@@ -1,28 +1,25 @@
-#!/bin/env python
-"""
-Run monthly artist charts for the year 2010 on your last.fm account.
-
-Requires python-httplib2 to be installed. Sorry about that.
-
-run the file as follows:
-python fetcher.py <yourusername>
-"""
-
-__author__ = 'Andy Theyers <andy.theyers@isotoma.com>'
-__docformat__ = 'restructuredtext en'
+# -*- coding: utf-8 -*-
 
 
-import httplib2
-import itertools
-import urllib
+### LastFetch v.0.0.9
+
+## AUTHOR: sugardrunk
+#  URL: http://sugardrunk.devio.us
+
+## ORIGIN: Andy Theyers
+#  E-MAIL: andy.theyers@isotoma.com
+
+
+import httplib2, itertools, urllib
 from xml.etree import ElementTree
-
 
 BASEURL = 'http://ws.audioscrobbler.com/2.0/user/%s/recenttracks.xml?limit=200&%s'
 
+print '\n'
+print 'PLEASE WAIT...'
+print '\n'
 
-class LastFetcher(object):
-    
+class LastFetch1(object):    
     def __init__(self, user, start=None, end=None):
         self.user = user
         self.start = start
@@ -76,43 +73,45 @@ class LastFetcher(object):
     
     def _getpages(self, xml):
         t = ElementTree.fromstring(xml)
-        return int(t.get('totalPages', 1))
-        
+        return int(t.get('totalPages', 1))        
     
 if __name__ == '__main__':
+
     from datetime import datetime
-    import sys
-    import time
+    import time, sys
+
     months = [
-        ('January', '2010-01-01', '2010-01-31',),
-        ('February', '2010-02-01', '2010-02-28',),
-        ('March', '2010-03-01', '2010-03-31',),
-        ('April', '2010-04-01', '2010-04-30',),
-        ('May', '2010-05-01', '2010-05-31',),
-        ('June', '2010-06-01', '2010-06-30',),
-        ('July', '2010-07-01', '2010-07-31',),
-        ('August', '2010-08-01', '2010-08-31',),
-        ('September', '2010-09-01', '2010-09-30',),
-        ('October', '2010-10-01', '2010-10-31',),
-        ('November', '2010-11-01', '2010-11-30',),
-        ('December', '2010-12-01', '2010-12-31',),
+        ('January', '2011-01-01', '2011-01-31',),
+        ('February', '2011-02-01', '2011-02-28',),
+        ('March', '2011-03-01', '2011-03-31',),
+        ('April', '2011-04-01', '2011-04-30',),
+        ('May', '2011-05-01', '2011-05-31',),
+        ('June', '2011-06-01', '2011-06-30',),
+        ('July', '2011-07-01', '2011-07-31',),
+        ('August', '2011-08-01', '2011-08-31',),
+        ('September', '2011-09-01', '2011-09-30',),
+        ('October', '2011-10-01', '2011-10-31',),
+        ('November', '2011-11-01', '2011-11-30',),
+        ('December', '2011-12-01', '2011-12-31',),        
+        ('Total', '2011-01-01', '2011-12-31',),
         ]
-    fmt = '%Y-%m-%d'
+
+    format1 = '%Y-%m-%d'
     user = sys.argv[1]
+
     for month in months:
         print month[0]
         print '='*len(month[0])
-        start = int(time.mktime(time.strptime(month[1], fmt)))
-        end = int(time.mktime(time.strptime(month[2], fmt)))
-        m = LastFetcher(user, start, end)
-        data = m.show(morethan=4)
+        start = int(time.mktime(time.strptime(month[1], format1)))
+        end = int(time.mktime(time.strptime(month[2], format1)))
+        m = LastFetch1(user, start, end)
+        data = m.show(morethan=0)
         totplays = 0
         for row in data:
             totplays += row[0]
         totartists = len(data)
-        print "Total artists this month:", totartists
-        print "Total tracks this month:", totplays
+        print 'Total artists :', totartists
+        print 'Total tracks :', totplays
         for row in data:
-            print row[0], ", ", row[1].encode('utf-8')
-        print "\n"
-    
+            print row[0],":",row[1].encode('utf-8')
+        print '\n'
